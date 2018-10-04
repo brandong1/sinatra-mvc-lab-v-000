2f2fa966d4f80f3dfd0777bbfa0df0005295869b
Brandon Green <brandon@brandonleeproductions.com>
2018-10-04 00:09:04 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,3 +1,12 @@
 class PigLatinizer < Sinatra::Base
-  
+  attr_reader :text
+
+  def initialize(text)
+    @text = text.downcase
+  end
+
+  def count_of_words
+    words = text.split(" ")
+    words.count
+  end
 end
