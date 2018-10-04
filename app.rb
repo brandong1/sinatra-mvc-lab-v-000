ce10911b3316110aa641cd0367e3e20e8803ea3b
Brandon Green <brandon@brandonleeproductions.com>
2018-10-04 00:04:41 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,4 +4,12 @@ class App < Sinatra::Base
   get '/'do
     erb :index
   end
+
+  post '/' do
+    text_from_user = params[:user_text]
+
+    @analyzed_text = PigLatinizer.new(params[:user_text])
+
+    erb :results
+  end
 end
